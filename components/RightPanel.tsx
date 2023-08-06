7dbab75250614903c31c3fd3215ed3547ea7fb72
= <nastya.minty@gmail.com>
2023-08-06 14:04:10 +0300

    fix for server current time bug

--- components/RightPanel.tsx
+++ components/RightPanel.tsx
@@ -3,12 +3,20 @@ import CountdownTimer from "./CountdownTimer";
 import { useState, useEffect } from "react";
 import { WeatherProps, TripProps } from "../types";
 type RightPanelProps = {
-    selectedTrip: TripProps | null; // Assuming TripProps is the type for your trip data
-    weatherToday: WeatherProps | null; // Assuming ForecastProps is the type for weather data
-  };
+  selectedTrip: TripProps | null; // Assuming TripProps is the type for your trip data
+  weatherToday: WeatherProps | null; // Assuming ForecastProps is the type for weather data
+};
 
-const RightPanel: React.FC<RightPanelProps> = ({ selectedTrip, weatherToday }) => {
-  const timeNow = new Date().getHours();
+const RightPanel: React.FC<RightPanelProps> = ({
+  selectedTrip,
+  weatherToday,
+}) => {
+  //const timeNow = new Date().getHours();
+  const [isDay, setIsDay] = useState(true); // Default to day, can be night too
+  useEffect(() => {
+    const timeNow = new Date().getHours();
+    setIsDay(timeNow >= 7 && timeNow < 19);
+  }, []);
   const weekdays = [
     "Sunday",
     "Monday",
@@ -32,8 +40,11 @@ const RightPanel: React.FC<RightPanelProps> = ({ selectedTrip, weatherToday }) =
   }, [weatherToday?.days[0].icon]); // Only run when day.icon changes
   return (
     <div
+      //   className={`${
+      //     timeNow >= 7 && timeNow <= 19 ? "bg-day-pattern" : "bg-night-pattern"
+      //   } bg-cover w-[450px] h-[110vh] flex flex-col justify-center items-center`}
       className={`${
-        timeNow >= 7 && timeNow <= 19 ? "bg-day-pattern" : "bg-night-pattern"
+        isDay ? "bg-day-pattern" : "bg-night-pattern"
       } bg-cover w-[450px] h-[110vh] flex flex-col justify-center items-center`}
     >
       {selectedTrip && weatherToday && (
